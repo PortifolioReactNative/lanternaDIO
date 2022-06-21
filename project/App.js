2ffcf21ecdedc9f12d6aeea03421e38d2b697e02
Cleomenes Bezerra Ferreira Junior <cleomenes2011@gmail.com>
2022-06-21 16:03:00 -0300

    update

--- project/App.js
+++ project/App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
 
-export default function App() {
+import imageOff from './assets/eco-light-off.png';
+import imageLight from './assets/eco-light.png';
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handlechangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+  useEffect(() => {
+    Torch.switchState(toggle);
+  }, [toggle]);
+
+  useEffect(() => {
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    })
+    return () => subscription.remove();
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={toggle ? style.containerLight : style.container}>
+      <TouchableOpacity 
+        onPress={ handlechangeToggle}> 
+        <Image style={toggle ? style.lightingOn : style.lightingOff} source={toggle ? imageLight : imageOff}/>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
+export default App;
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  containerLight : {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  lightingOn:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+
+  lightingOff:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 150,
+    height: 150,
   },
 });
